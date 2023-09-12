5228cf26fa7523bf4cc66c5fb7e17682b252b192
Mafuzur560 <mrridoy461@gmail.com>
2023-09-12 17:42:25 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Blogs from './Components/Blogs/Blogs'
+import Header from './Components/Header/Header'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <Header></Header>
+      <Blogs></Blogs>
+      
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-4xl text-red-300'>Vite + React</h1>
+        </div>
+    );
+};
+
+export default Header;
